bb7417e2bfd196e05e20c7d66e22c4f0baa80b13
ChaseRensberger <chasehrensberger@gmail.com>
2024-03-01 23:16:46 -0500

    improve line detection

--- laneDetection.py
+++ laneDetection.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
+import math
 
 
 def region_of_interest(img, vertices):
@@ -19,6 +20,30 @@ def region_of_interest(img, vertices):
     masked_image = cv.bitwise_and(img, mask)
     return masked_image
 
+def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
+    # If there are no lines to draw, exit.
+    if lines is None:
+        return
+    # Make a copy of the original image.
+    img = np.copy(img)
+    # Create a blank image that matches the original in size.
+    line_img = np.zeros(
+        (
+            img.shape[0],
+            img.shape[1],
+            3
+        ),
+        dtype=np.uint8,
+    )
+    # Loop over all lines and draw them on the blank image.
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv.line(line_img, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness)
+    # Merge the image with the lines onto the original.
+    img = cv.addWeighted(img, 0.8, line_img, 1.0, 0.0)
+    # Return the modified image.
+    return img
+
 cap = cv.VideoCapture("labeled/0.hevc")
 ret, image = cap.read()
 
@@ -34,14 +59,14 @@ region_of_interest_vertices = [
 
 plt.figure()
 plt.imshow(image)
-plt.show()
-
-
 gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
 cannyed_image = cv.Canny(gray_image, 100, 200)
 cropped_image = region_of_interest(
     cannyed_image,
-    np.array([region_of_interest_vertices], np.int32)
+    np.array(
+        [region_of_interest_vertices],
+        np.int32
+    ),
 )
 lines = cv.HoughLinesP(
     cropped_image,
@@ -52,8 +77,45 @@ lines = cv.HoughLinesP(
     minLineLength=40,
     maxLineGap=25
 )
-
-line_image = draw_lines(image, lines) # <---- Add this call.
+left_line_x = []
+left_line_y = []
+right_line_x = []
+right_line_y = []
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        slope = (y2 - y1) / (x2 - x1) # <-- Calculating the slope.
+        if math.fabs(slope) < 0.5: # <-- Only consider extreme slope
+            continue
+        if slope <= 0: # <-- If the slope is negative, left group.
+            left_line_x.extend([x1, x2])
+            left_line_y.extend([y1, y2])
+        else: # <-- Otherwise, right group.
+            right_line_x.extend([x1, x2])
+            right_line_y.extend([y1, y2])
+min_y = image.shape[0] * (3 / 5) # <-- Just below the horizon
+max_y = image.shape[0] # <-- The bottom of the image
+poly_left = np.poly1d(np.polyfit(
+    left_line_y,
+    left_line_x,
+    deg=1
+))
+left_x_start = int(poly_left(max_y))
+left_x_end = int(poly_left(min_y))
+poly_right = np.poly1d(np.polyfit(
+    right_line_y,
+    right_line_x,
+    deg=1
+))
+right_x_start = int(poly_right(max_y))
+right_x_end = int(poly_right(min_y))
+line_image = draw_lines(
+    image,
+    [[
+        [left_x_start, max_y, left_x_end, min_y],
+        [right_x_start, max_y, right_x_end, min_y],
+    ]],
+    thickness=5,
+)
 plt.figure()
 plt.imshow(line_image)
 plt.show()
