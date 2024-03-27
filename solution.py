74441d775d90ae6b78662eb50131a398090bf0e4
ChaseRensberger <chasehrensberger@gmail.com>
2024-03-26 22:20:34 -0400

    bring back old approach

--- solution.py
+++ solution.py
@@ -14,7 +14,30 @@ def write_tuples_to_file(array_of_tuples, subdirectory, file_name):
             file.write(line)
 
 def visualize_lane_lines(frame, ll_seg_out, threshold=0.95):
-   pass 
+    lane_line_probs, _ = torch.max(ll_seg_out, dim=1)
+    lane_line_mask = lane_line_probs.squeeze().cpu().numpy() > threshold
+    lane_line_mask_resized = cv2.resize(lane_line_mask.astype(np.uint8), (frame.shape[1], frame.shape[0]))
+
+    # Convert binary mask to a 3 channel image to create a color overlay
+    lane_line_overlay = np.zeros_like(frame)
+    lane_line_overlay[:, :, :] = 0  # Reset all channels to zero - No tint
+    lane_line_overlay[lane_line_mask_resized == 1, :] = [0, 255, 0]  # Green color for the lane lines
+
+    # Blend the overlay with the original frame
+    visualized_frame = cv2.addWeighted(frame, 1, lane_line_overlay, 0.4, 0)
+
+    return visualized_frame
+
+    # lines = cv2.HoughLinesP(lane_line_mask_resized.astype(np.uint8), 1, np.pi/180, threshold=100, minLineLength=1000, maxLineGap=5)
+
+    # if lines is not None:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # else:
+    #     print("No lines detected")
+
+    # return frame
 
 def processVideo(video_path, model, transform):
     cap = cv2.VideoCapture(video_path)
@@ -35,8 +58,8 @@ def processVideo(video_path, model, transform):
         with torch.no_grad():
             det_out, da_seg_out, ll_seg_out = model(frame_tensor)
         
-        # lane_line_frame = visualize_lane_lines(frame, ll_seg_out)
-        cv2.imshow("frame", frame)
+        lane_lines_frame = visualize_lane_lines(frame, ll_seg_out)
+        cv2.imshow("frame", lane_lines_frame)
         if cv2.waitKey(50) & 0xFF == ord('q'):
             break
 
